6ddfdcbe4edeafac9a76674dfe93f3ad3c9eaaeb
kish202 <tania2020076@gmail.com>
2025-04-03 22:23:52 +0530

    modify

--- frontend/src/global/Topbar.jsx
+++ frontend/src/global/Topbar.jsx
@@ -25,11 +25,46 @@ const Topbar = () => {
           backgroundColor={colors.primary[400]}
           borderRadius="3px">
 <InputBase  sx={{ml:2, flex:1}} placeholder="Search"/>
-        </Box>
-<IconButton></IconButton>
 
-    </Box>
+
+
+<IconButton type="button" sx={{p:1}}>
+
+    <SearchIcon/>
+</IconButton>
+  </Box>
+
+
+
+<Box display="flex">  
+    <IconButton onClick={colorMode.toggleColorMode}>
+        {
+            theme.palette.mode === 'dark' ? (<DarkModeOutlinedIcon/>) : (<LightModeOutlinedIcon/>)
+        }
+
+    </IconButton>
+
+<IconButton>
+<NotificationsOutlinedIcon/>
+</IconButton>
+
+<IconButton>
+<SettingsOutlinedIcon/>
+</IconButton>
+<IconButton>
+    <PersonOutlinedIcon/>
+
+</IconButton>
+</Box>
+
+
+
+      </Box>
+
+
+
 )
+// npm install @mui/material @emotion/react @emotion/styled --legacy-peer-deps
 
 }
 export default Topbar;
